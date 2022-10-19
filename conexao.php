5ca2b649362bbd205d475da61cc664b17a0676d6
RyanCordeiro <110617404+RyanCordeiro@users.noreply.github.com>
2022-10-19 10:19:48 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,9 +2,9 @@
 
 // $pdo = new PDO('mysql:host=127.0.0.1;dbname=u468517930_next','u468517930_next','Pietro.2007');
 
-define('HOST', '127.0.0.1:3306');
+define('HOST', 'localhost');
 define('USUARIO', 'u468517930_next');
-define('SENHA', 'Pietro.2007');
+define('SENHA', '');
 define('DB', 'u468517930_next');
 $conexao = mysqli_connect(HOST , USUARIO, SENHA, DB) or die('Falhou Sua conexão com o banco de dados');
 
